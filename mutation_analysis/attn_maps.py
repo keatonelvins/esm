4bcf8486610a4e9963ce1817e7f76f65b2c07db7
keatonelvins <keatone@gmail.com>
2023-04-13 12:37:03 -0700

    added dataloader to precompute attn scores

--- mutation_analysis/attn_maps.py
+++ mutation_analysis/attn_maps.py
@@ -1,17 +1,23 @@
 import torch
 import esm
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # Load ESM-2 model
 model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
 batch_converter = alphabet.get_batch_converter()
 model.eval()  # disables dropout for deterministic results
 
+# Example usage
 seq = "DVLTFNSAAYNNK"
-# Prepare data
 data = [
     ("protein1", seq)
 ]
+
+# Load data and convert to list of tuples, i.e. ('0000000000111', DVLTFNSAAYDKR)
+df = pd.read_csv("mutation_analysis\MOESM7_ESM.xlsx")
+data = list(df[['genotype', 'sequence']].to_records(index=False))
+
 batch_labels, batch_strs, batch_tokens = batch_converter(data)
 batch_lens = (batch_tokens != alphabet.padding_idx).sum(1)
 
@@ -19,17 +25,19 @@ batch_lens = (batch_tokens != alphabet.padding_idx).sum(1)
 with torch.no_grad():
     results = model(batch_tokens, repr_layers=[33], return_contacts=True)
 
-num_layers, num_heads, seq_length = results['attentions'].shape[1:4]
-fig, axs = plt.subplots(4, 5, figsize=(10, 8))
+torch.save(results['attentions'], 'precomputed_attn_maps.pt')
 
-for l in range(num_heads):
-    for ax, head in zip(axs.flatten(), results['attentions'][0, l]):
-        ax.imshow(head)
-        ax.axis('off')
+# num_layers, num_heads, seq_length = results['attentions'].shape[1:4]
+# fig, axs = plt.subplots(4, 5, figsize=(10, 8))
 
-    fig.subplots_adjust(hspace=0.3, wspace=0.3)
-    plt.suptitle(f'Attn. Maps for {seq} (Layer {l} Heads 1-20)', fontsize=16)
-    plt.savefig(f'mutation_analysis/attn_maps/layer_{l}')
+# for l in range(num_heads):
+#     for ax, head in zip(axs.flatten(), results['attentions'][0, l]):
+#         ax.imshow(head)
+#         ax.axis('off')
+
+#     fig.subplots_adjust(hspace=0.3, wspace=0.3)
+#     plt.suptitle(f'Attn. Maps for {seq} (Layer {l} Heads 1-20)', fontsize=16)
+#     plt.savefig(f'mutation_analysis/attn_maps/layer_{l}')
 
 # # Look at the unsupervised self-attention map contact predictions
 # for (_, seq), tokens_len, attention_contacts in zip(data, batch_lens, results["contacts"]):
