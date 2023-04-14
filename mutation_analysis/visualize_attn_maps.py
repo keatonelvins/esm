6b9418f9fb6dda84868aec494a83031cb8475f8b
keatonelvins <keatone@gmail.com>
2023-04-13 18:20:32 -0700

    cleaned up visualization code

--- mutation_analysis/visualize_attn_maps.py
+++ mutation_analysis/visualize_attn_maps.py
@@ -3,14 +3,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Load precomputed attn. maps and sequence labels
-batch_num = 0
-results = torch.load(f'mutation_analysis/attn_maps/precomputed_attn_maps_{batch_num}.pt')
+results = torch.load(f'mutation_analysis/attn_maps/precomputed_attn_maps_0.pt')
 sequences = pd.read_excel("mutation_analysis/MOESM7_ESM.xlsx")['sequence']
 
-# Plot attn. maps across all layers for sequence i
+# Plot attn. maps across all layers for sequence i (make sure correct .pt file is loaded)
 i = 0
 n, num_layers, num_heads, seq_length = results.shape[:4]
-fig, axs = plt.subplots(4, 5, figsize=(10, 8))
+fig, axs = plt.subplots(4, num_heads//4, figsize=(10, 8))
 
 for l in range(num_layers):
     for ax, head in zip(axs.flatten(), results[i % n, l]):
@@ -18,7 +17,5 @@ for l in range(num_layers):
         ax.axis('off')
 
     fig.subplots_adjust(hspace=0.3, wspace=0.3)
-    plt.suptitle(f'Attn. Maps for {sequences[i]} (Layer {l} Heads 1-20)', fontsize=16)
-    # plt.savefig(f'mutation_analysis/attn_map_plots/layer_{l}')
-
-plt.show()
+    plt.suptitle(f'Attn. Maps for {sequences[i]} (Layer {l+1} Heads 1-20)', fontsize=16)
+    plt.savefig(f'mutation_analysis/attn_map_plots/seq_{i}_layer_{l+1}')
